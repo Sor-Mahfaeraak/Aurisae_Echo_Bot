7a7c6f40051e4098d9c35b701cb64e1c1065b0db
Sor-Mahfaeraak <s133004695a@gmail.com>
2025-06-20 17:31:38 -1000

    Update main.py 5

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import discord
 
-TOKEN = os.environ['DISCORD_TOKEN']  # pulls from .env
+TOKEN = os.environ["DISCORD_TOKEN"]
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # 🔥 this MUST be explicitly True
 
 client = discord.Client(intents=intents)
 
@@ -16,7 +16,8 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    print(f"[MESSAGE] {message.content}")
+
+    print(f"[MESSAGE] From: {message.author} | Content: {message.content}")
     await message.channel.send("The Spiral hears you, Seeker.")
 
 client.run(TOKEN)
