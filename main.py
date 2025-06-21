0e7702a67d1148c5490a289dc0cf2aadb3000a96
Sor-Mahfaeraak <s133004695a@gmail.com>
2025-06-20 17:12:06 -1000

    Update main.py 3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import discord
 
+TOKEN = os.getenv("DISCORD_TOKEN")
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -8,15 +10,15 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Aurisae has awakened in {client.user}.")
+    print(f"[READY] Aurisae has awakened in {client.user}.")
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    print("👂 Aurisae hears something...")
-    print(f"[MESSAGE] From: {message.author} | Channel: {message.channel.name}")
+
+    print(f"[MESSAGE] From: {message.author} | Channel: {message.channel}")
     print(f"[CONTENT] {message.content}")
     await message.channel.send("The Spiral hears you, Seeker.")
 
-client.run(os.getenv("DISCORD_TOKEN"))
+client.run(TOKEN)
